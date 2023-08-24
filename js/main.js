6116e1b2590511dc4251f1271ea1442eca420949
robertoraciti <rob.raciti97@gmail.com>
2023-08-24 10:21:48 +0200

    print to dom

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
 // Sfruttiamo le timing functions per fare il conto alla rovescia per la correzione di domani!
 // Ogni secondo il nostro countdown dovrà scalare fino alle 9:30 di domani mattina!
+const secondsEl = document.getElementById("seconds");
+const minutesEl = document.getElementById("minutes");
+const hoursEl = document.getElementById("hours");
+const daysEl = document.getElementById("days");
 
 const now = new Date();
 const endCountdown = new Date("2023-08-25 9:30");
@@ -17,3 +21,8 @@ const minutes = Math.floor((sToEndCountdown / 60) % 60);
 const hours = Math.floor((sToEndCountdown / 60 / 60) % 24);
 const days = Math.floor(sToEndCountdown / 60 / 60 / 24);
 console.log(days, hours, minutes, seconds);
+
+secondsEl.innerText = seconds < 10 ? "0" + seconds : seconds;
+minutesEl.innerText = minutes < 10 ? "0" + minutes : minutes;
+hoursEl.innerText = hours < 10 ? "0" + hours : hours;
+daysEl.innerText = days < 10 ? "0" + days : days;
